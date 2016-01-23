6e4b916a2824bbda37fea5a5fd646468cd598aed
Benjamin Singer <benjaminjsinger@gmail.com>
2016-01-23 16:18:32 -0600

    updated to accordion with multiple streams

--- myjs.js
+++ myjs.js
@@ -1,55 +1,46 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap']);
 
 app.controller('Controller', ["$scope", "$http", "$timeout", "$q", function($scope, $http, $timeout, $q) {
     var self = this;
 
     self.getNHL = function(){
-        var promises = [];
-        $http.get('//www.reddit.com/user/NHLStreamsBot/submitted.json?&sort=new').success(function(data){
+        $http.get('//www.reddit.com/r/NHLStreams/search.json?q=Game+Thread%3A&sort=news&restrict_sr=on&t=day&limit=100').success(function(data){
             var date = (new Date());
             date.setHours(0);
             date.setMinutes(0);
             date.setMilliseconds(0);
             self.nhl = data.data.children.filter(function(child){
-                return (child.data.created_utc * 1000) > date.getTime();
+                return (child.data.created_utc * 1000) > date.getTime() && child.data.title.indexOf('Game Thread') > -1;
             });
 
             self.nhl.forEach(function(game){
-                var p  = self.getYoutube(game.data.id);
-                promises.push(p);
-                p.success(function(data){
-                    console.log(data[1].data.children);
-
+                $http.get('//www.reddit.com/r/NHLStreams/comments/' + game.data.id + '.json?&sort=top&limit=5').success(function(data){
                     game.bestStreams = data[1].data.children.map(function(child){
                         if(child.data.body){
-                            // return child.data.body.split(' ').filter(function(s){
-                            //     return s.indexOf('http') > -1;
-                            // });
                             var r;
                             var result = URI.withinString(child.data.body, function(url) {
                                 r = url.replace('*', '').replace('*', '');
                                 return url;
                             });
-                            return r;
+                            if(r){
+                                return r;
+                            }
                         }
                     }) || [];
+                    game.bestStreams = game.bestStreams.filter(function(url){
+                        return url !== undefined;
+                    });
+                    console.log(game.bestStreams);
                 }).error(function(){
                     console.log('Error getting best streams...');
                 });
+                game.data.title = game.data.title.replace("Game Thread:", "");
             });
         }).error(function(err){
             console.error(err);
         });
-
-        $q.all(promises).then(function(){
-            // $scope.$apply();
-        });
     };
     self.getNHL();
-
-    self.getYoutube = function(id){
-        return $http.get('//www.reddit.com/r/NHLStreams/comments/' + id + '.json?&sort=top&limit=5');
-    };
 
     // self.search = function(search){
     //     if(self.timeoutId) $timeout.cancel(self.timeoutId);
