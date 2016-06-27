f5424fc3c33b2906ea6fff6fae582c7d273d66a5
Benjamin Singer <benjaminjsinger@gmail.com>
2016-06-26 20:10:07 -0500

    add moar sports

--- myjs.js
+++ myjs.js
@@ -2,19 +2,21 @@ var app = angular.module('app', ['ui.bootstrap']);
 
 app.controller('Controller', ["$scope", "$http", "$timeout", "$q", function($scope, $http, $timeout, $q) {
     var self = this;
+    self.subs = ["NHLStreams", "soccerstreams", "nbastreams", "mlbstreams", "nflstreams"];
 
-    self.getNHL = function(){
-        $http.get('//www.reddit.com/r/NHLStreams/search.json?q=Game+Thread%3A&sort=news&restrict_sr=on&t=day&limit=100').success(function(data){
+    self.getStreams = function(sub){
+        self.sub = sub;
+        $http.get('//www.reddit.com/r/' + sub + '/search.json?q=Game+Thread%3A&sort=news&restrict_sr=on&t=day&limit=100').success(function(data){
             var date = (new Date());
             date.setHours(0);
             date.setMinutes(0);
             date.setMilliseconds(0);
-            self.nhl = data.data.children.filter(function(child){
+            self.games = data.data.children.filter(function(child){
                 return (child.data.created_utc * 1000) > date.getTime() && child.data.title.indexOf('Game Thread') > -1;
             });
 
-            self.nhl.forEach(function(game){
-                $http.get('//www.reddit.com/r/NHLStreams/comments/' + game.data.id + '.json?&sort=top&limit=5').success(function(data){
+            self.games.forEach(function(game){
+                $http.get('//www.reddit.com/r/' + sub + '/comments/' + game.data.id + '.json?&sort=top&limit=5').success(function(data){
                     game.bestStreams = data[1].data.children.map(function(child){
                         if(child.data.body){
                             var r;
@@ -30,7 +32,6 @@ app.controller('Controller', ["$scope", "$http", "$timeout", "$q", function($sco
                     game.bestStreams = game.bestStreams.filter(function(url){
                         return url !== undefined;
                     });
-                    console.log(game.bestStreams);
                 }).error(function(){
                     console.log('Error getting best streams...');
                 });
@@ -40,19 +41,5 @@ app.controller('Controller', ["$scope", "$http", "$timeout", "$q", function($sco
             console.error(err);
         });
     };
-    self.getNHL();
-
-    // self.search = function(search){
-    //     if(self.timeoutId) $timeout.cancel(self.timeoutId);
-    //     if(search.length > 0){
-    //         self.timeoutId = $timeout(function() {
-    //             $http.get('https://api.spotify.com/v1/search?q=' + search + '&type=track&limit=50').success(function(data){
-    //                 self.data = data.tracks.items;
-    //             }).error(function(err){
-    //                 console.error(err);
-    //             });
-    //         }, 250);
-    //     }
-    // };
-
+    self.getStreams(self.subs[0]);
 }]);
